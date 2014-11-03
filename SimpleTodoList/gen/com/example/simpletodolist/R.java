8f24ac9a1692b256dc7e063fe71b0d8ec3952524
Varadarajan Krishnan <rajesh_k112@yahoo.co.in>
2014-11-03 22:53:38 +0530

    Pushing the android training

--- SimpleTodoList/gen/com/example/simpletodolist/R.java
+++ SimpleTodoList/gen/com/example/simpletodolist/R.java
@@ -14,12 +14,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int btnAddItem=0x7f060002;
-        public static final int etNewItem=0x7f060001;
-        public static final int lvItems=0x7f060000;
+        public static final int btnAddItem=0x7f070002;
+        public static final int etNewItem=0x7f070003;
+        public static final int lvItems=0x7f070001;
+        public static final int textView1=0x7f070000;
     }
     public static final class layout {
-        public static final int activity_to_do=0x7f030000;
+        public static final int activity_todo=0x7f030000;
+    }
+    public static final class menu {
+        public static final int todo=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- SimpleTodoList/src/com/example/simpletodolist/ToDoActivity.java
+++ SimpleTodoList/src/com/example/simpletodolist/ToDoActivity.java
@@ -2,27 +2,28 @@ package com.example.simpletodolist;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.*;
 import android.widget.*;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.view.*;
 import java.io.*;
-
 import org.apache.commons.io.FileUtils;
 
 
-public class ToDoActivity extends Activity {
-    ArrayList<String> items;
+public class TodoActivity extends Activity {
+	ArrayList<String> items;
     ArrayAdapter<String> itemsAdapter;
     ListView lvItems;
-    
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_to_do);
+    	readItems();
+    	itemsAdapter = new ArrayAdapter<String>(getBaseContext(),android.R.layout.simple_list_item_1,items);
+    	super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo);
         lvItems = (ListView) findViewById(R.id.lvItems);
         items = new ArrayList<String>();
         itemsAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, items);
@@ -30,28 +31,33 @@ public class ToDoActivity extends Activity {
         items.add("First Item");
         items.add("Second Item");
         setupListViewListener();
-        readItems();
-    	itemsAdapter = new ArrayAdapter<String>(getBaseContext(),android.R.layout.simple_list_item_1,items);
     }
-    
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	getMenuInflater().inflate(R.menu.todo,menu);
+    	return true;
+    }
+
     public void addTodoItem(View v) {
     	EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
     	itemsAdapter.add(etNewItem.getText().toString());
     	etNewItem.setText("");
     	saveItems();
     }
-    
+
     private void setupListViewListener() {
     	lvItems.setOnItemLongClickListener(new OnItemLongClickListener() {
     		@Override
     		public boolean onItemLongClick(AdapterView<?> parent,View view, int position, long rowId) {
     			items.remove(position);
     			itemsAdapter.notifyDataSetChanged();
+    			saveItems();
     			return true;
     		}
     	});
     }
-    
+
     private void readItems() {
     	File filesDir = getFilesDir();
     	File todoFile = new File(filesDir, "todo.txt");
@@ -72,9 +78,6 @@ public class ToDoActivity extends Activity {
     		e.printStackTrace();
     	}
     }
-   
- 
-}    
-
-
+    
+}
 
